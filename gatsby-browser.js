e538030f0bb7fc8343810258161a810557f39525
Guillaume Briday <guillaumebriday@gmail.com>
2019-03-12 00:52:25 +0100

    Configuring Gatsby

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,72 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faRss,
+  faSearch,
+  faUser,
+  faComments,
+  faCameraRetro,
+  faMobileAlt,
+  faChevronLeft,
+  faChevronRight,
+  faArrowLeft,
+  faArrowRight,
+  faSpinner,
+  faBook,
+  faGlobeEurope,
+  faAtom,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faCalendarAlt,
+  faClock,
+  faKeyboard,
+  faCheckCircle,
+} from '@fortawesome/free-regular-svg-icons'
+import {
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faAmazon,
+  faPaypal,
+} from '@fortawesome/free-brands-svg-icons'
 
-// You can delete this file if you're not using it
+import './src/styles/tailwind.scss'
+import './src/styles/index.scss'
+import SmoothScroll from 'smooth-scroll'
+import 'typeface-roboto'
 
-import "./src/styles/index.scss"
+library.add(
+  faRss,
+  faSearch,
+  faUser,
+  faCalendarAlt,
+  faKeyboard,
+  faCameraRetro,
+  faComments,
+  faMobileAlt,
+  faBook,
+  faGlobeEurope,
+  faAtom,
+  faClock,
+  faChevronLeft,
+  faChevronRight,
+  faCheckCircle,
+  faArrowLeft,
+  faArrowRight,
+  faSpinner,
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faAmazon,
+  faPaypal
+)
+
+export const onRouteUpdate = () => {
+  new SmoothScroll('a[href*="#"]', {
+    duration: 350,
+    speedAsDuration: true,
+    offset: 30,
+    updateURL: false,
+  })
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,58 @@
 const tailwindcss = require('tailwindcss')
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+// gatsby-config.js
+const myQuery = `{
+  allMarkdownRemark(sort: {fields: [fields___date], order: DESC}, filter: {frontmatter: {layout: {eq: "post"}}}) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+        }
+        internal {
+          type
+          contentDigest
+          owner
+        }
+      }
+    }
+  }
+}
+`
+
+const queries = [
+  {
+    query: myQuery,
+    transformer: ({ data }) =>
+      data.allMarkdownRemark.edges.map(({ node }) => node),
+    indexName: 'blog',
+  },
+]
 
 module.exports = {
   siteMetadata: {
     title: `Guillaume Briday`,
     description: `Ce blog est à propos des nouvelles technologies, de mes humeurs, de développement Web, de photos... En bref, de tout ce dont j'ai envie de parler.`,
-    siteUrl: `https://guillaumebriday.fr`
+    siteUrl: `https://guillaumebriday.fr`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_API_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+      },
+    },
+    'gatsby-transformer-yaml',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,16 +61,45 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: `images`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/data`,
+        name: 'comments',
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-copy-linked-files`,
           {
             resolve: `gatsby-remark-images`,
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
+              maxWidth: 800,
+              withWebp: true,
+              quality: 100,
+              showCaptions: true,
+              linkImagesToOriginal: false,
+              backgroundColor: 'transparent',
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: true,
             },
           },
         ],
@@ -34,30 +109,110 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Guillaume Briday',
+        short_name: 'Guillaume Briday',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#6574CD',
+        theme_color: '#6574CD',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/icon.png',
       },
     },
     'gatsby-plugin-offline',
     'gatsby-plugin-sitemap',
+    'gatsby-plugin-robots-txt',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.fields.date,
+                  url: [site.siteMetadata.siteUrl, edge.node.fields.slug].join(
+                    '/'
+                  ),
+                  guid: [site.siteMetadata.siteUrl, edge.node.fields.slug].join(
+                    '/'
+                  ),
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                })
+              })
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [fields___date] },
+                filter: {frontmatter: { layout: { eq: "post" } }}
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                      date
+                    }
+                    frontmatter {
+                      layout
+                      title
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: '/feed.xml',
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/layouts/index.jsx`),
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-54003772-2`,
+        anonymize: true,
+        respectDNT: true,
       },
     },
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        postCssPlugins: [
-          tailwindcss('./tailwind.js'),
-          require('autoprefixer')
-        ]
+        postCssPlugins: [tailwindcss('./tailwind.js'), require('autoprefixer')],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        tailwind: true,
+        ignore: ['src/styles/index.scss'],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        generateMatchPathRewrites: false,
       },
     },
   ],
