beb67b7fc7fecdb3360760e81e3f62048cf89b6e
Guillaume Briday <guillaumebriday@gmail.com>
2019-03-23 22:46:13 +0100

    Importing ReactMarkdown with import modules

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CommentForm from './CommentForm'
-const ReactMarkdown = require('react-markdown')
+import ReactMarkdown from 'react-markdown'
 
 export default ({ post, allCommentsYaml }) => {
   const comments = allCommentsYaml ? allCommentsYaml.edges : []
